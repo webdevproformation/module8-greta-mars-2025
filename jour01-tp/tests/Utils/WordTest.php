99814172ff35f106fe38180738e3758d91b54e6b
Malik H <malik.h@webdevpro.net>
2025-03-11 11:09:47 +0100

    presention dataprovider phpunit

--- jour01-tp/tests/Utils/WordTest.php
+++ jour01-tp/tests/Utils/WordTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Utils\Word;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class WordTest extends TestCase{
@@ -24,6 +25,22 @@ class WordTest extends TestCase{
         $this->assertSame($resultat , ["a","a", "a", "a"]);
     }
 
+    #[DataProvider("voyelleProvider")]
+    public function test_voyelle2(string $text , array $expected){
 
+        $word = new Word();
+        $resultat = $word->voyelle($text);
+        $this->assertSame($resultat , $expected);
+
+    }
+    public static function voyelleProvider(){
+        return [
+           "cas bonjour"     => [ "bonjour"  , ["o","o", "u"]       ],
+           "cas string vide" => [ ""         , [ ]                  ],
+           "cas hello"       => [ "hello"    , ["e", "o" ]          ],
+           "cas rrrrrrrr"    => [ "rrrrrrrr" , []                   ],
+           "cas aaaa"        =>  [ "aaaa"    , ["a","a", "a", "a"]  ]
+        ];
+    }
 }
                        
---
+++ jour02/01-dataprovider.md
@@ -0,0 +1,3 @@
+# dataprovider 
+
+<https://docs.phpunit.de/en/10.5/writing-tests-for-phpunit.html>
