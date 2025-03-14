8a7afe0e761e7b9b872ca834d34cd6525e8ae836
Malik H <malik.h@webdevpro.net>
2025-03-14 10:34:37 +0100

    react + symfony

---
+++ jour04-back/src/Controller/RecetteController.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace App\Controller ;
+
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class RecetteController extends AbstractController{
+
+    //  https://127.0.0.1:8000 
+    #[Route("/", name: "json_all_recettes")]
+    public function getAll(){
+        $recettes = [
+            [ 
+                "id" => 1,
+                "nom" => "crêpe fromage",
+                "prix" => 2,
+                "ingredients" => [ "oeuf", "lait" , "beurre" ]
+            ],
+            [
+                "id" => 2,
+                "nom" => "pizza",
+                "prix" => 5,
+                "ingredients" => [ "tomate", "poivron" ]
+            ]
+        ];
+        return new JsonResponse($recettes); 
+    }
+
+
+}
---
+++ jour04/00-sommaire.md
@@ -0,0 +1,13 @@
+## sommaire de la dernière journée
+
+- exemple de projet React + Symfony 
+- Responsive à la main + bootstrap
+- SEO
+- 2 qcms 
+
+
+# React 1 seule page + Symfony 1 seul controller avec 1 seule méthode
+
+symfony new jour04-back 
+cd jour04-back
+symfony serve 
